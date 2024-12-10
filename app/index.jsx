552d698d001766793377d41a576d24198c90aeeb
Ozan Sevkin <sevkinozan@gmail.com>
2024-12-10 14:24:22 +1100

    feat: setup project

---
+++ app/index.jsx
@@ -0,0 +1,5 @@
+import Onboarding from "../screens/Onboarding";
+
+export default function Index() {
+  return <Onboarding />;
+}
---
+++ constants/Colors.js
@@ -0,0 +1,18 @@
+// Below are the colors that are used in the app.
+
+const Colors = {
+  primary: {
+    green: "#495E57",
+    yellow: "#F4CE14",
+  },
+  secondary: {
+    orange: "#EE9972",
+    beige: "#FBDABB",
+    gray100: "#EDEFEE",
+    gray200: "#DEE3E9",
+    gray300: "#CBD2D9",
+    gray700: "#333333",
+  },
+};
+
+export default Colors;
